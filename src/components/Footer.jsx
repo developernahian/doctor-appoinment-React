a0c8549038efbedfc510518bcb3448b031acd9e0
developernahian <developer.nahian@gmail.com>
2024-11-11 02:51:55 +0600

    footer component added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,24 +3,39 @@ import { assets } from "../assets/assets"
 
 const Footer = () => {
   return (
-    <div>
-      <div>
+    <div className="md:mx-10">
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
         {/* ========== Left Section ========== */}
         <div>
-            <img src={assets.logo} alt="logo" />
-            <p>
+            <img className="mb-5 w-40" src={assets.logo} alt="logo" />
+            <p className="w-full md:w-2/3 text-gray-600 leading-6">
             Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
             </p>
         </div>
         {/* ========== Center Section ========== */}
         <div>
-
+            <p className="text-xl font-medium mb-5">COMPANY</p>
+            <ul className="flex flex-col gap-2 text-gray-600">
+                <li>Home</li>
+                <li>About us</li>
+                <li>Contact us</li>
+                <li>Privacy policy</li>
+            </ul>
         </div>
         {/* ========== Right Section ========== */}
         <div>
-
+            <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+            <ul className="flex flex-col gap-2 text-gray-600">
+                <li>+01242124165</li>
+                <li>gmail@gmail.com</li>
+            </ul>
         </div>
       </div>
+
+        {/* Coppyright Text */}
+        <hr />
+        <p className="py-5 text-sm text-center">Copyright © 2024 abc - All Right Reserved.</p>
+
     </div>
   )
 }
