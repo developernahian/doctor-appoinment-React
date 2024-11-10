6950ce83dc1fb920d3ae8ef3922900cff8d03e38
developernahian <developer.nahian@gmail.com>
2024-11-10 11:13:43 +0600

    update nav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,13 @@
 import { NavLink, useNavigate } from 'react-router-dom'
 import {assets} from '../assets/assets'
+import { useState } from 'react';
 
 const Navbar = () => {
 
     const navigate = useNavigate();
+
+    const [showMenu, setShowMenu] = useState(false)
+    const [token, setToken] = useState(true)
 
   return (
     <div className='flex items-center justify-between text-sm py-4 border-b border-b-gray-400'>
@@ -33,7 +37,24 @@ const Navbar = () => {
       </ul>
 
       <div className='flex items-center gap-4'>
-            <button onClick={()=>navigate('/login')}  className='bg-primary text-white px-8 py-3 rounded-full font-light hidden  md:block'>Create account</button>
+
+            {
+                token
+                ? <div className='flex items-center gap-2 cursor-pointer group relative'>
+                    <img src={assets.profile_pic}  className='w-8 rounded-full' alt='profile'/>
+                    <img src={assets.dropdown_icon} className='w-2.5' alt='dropdown'/>
+                    <div className='absolute top-0 right-0 pt-14 text-base text-gray-600 z-20 hidden group-hover:block'>
+                        <div className='min-w-48 bg-stone-100 rounded flex flex-col gap-4 p-4'>
+                            <p onClick={()=>navigate('my-profile')} className='hover:text-black cursor-pointer'>My Profile</p>
+                            <p onClick={()=>navigate('my-appointments')} className='hover:text-black cursor-pointer'>My Appointment</p>
+                            <p onClick={()=>setToken(false)} className='hover:text-black cursor-pointer'>Logout</p>
+                        </div>
+                    </div>
+                </div>
+                : <button onClick={()=>navigate('/login')}  className='bg-primary text-white px-8 py-3 rounded-full font-light hidden  md:block'>Create account</button>
+            }
+
+            
         </div>
     </div>
   )
