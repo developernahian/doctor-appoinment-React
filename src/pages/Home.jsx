0e32b6632917bfa7b1d60e784aa5ec33f1682e6e
developernahian <developer.nahian@gmail.com>
2024-11-10 12:34:56 +0600

    speciality component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import Header from "../components/header"
+import SpecialityMenu from "../components/specialityMenu"
 
 
 const Home = () => {
   return (
     <div>
       <Header/>
+      <SpecialityMenu/>
     </div>
   )
 }
